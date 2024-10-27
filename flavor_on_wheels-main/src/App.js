e9fd956b5c292b657a708030ce7b75ccd00d1598
Daniel Orina <65708288+danielorina750@users.noreply.github.com>
2024-10-27 14:49:39 +0300

    Add files via upload

---
+++ flavor_on_wheels-main/src/App.js
@@ -0,0 +1,21 @@
+/** @format */
+
+import Navbar from "./components/Navbar/Nav.jsx";
+import Hero from "./components/Hero/Hero.jsx";
+import Cards from "./components/Cards/Cards.jsx";
+import Food from "./components/Food/Food.jsx";
+import Category from "./components/Category/Category.jsx";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Cards />
+      <Food />
+      <Category />
+    </div>
+  );
+}
+
+export default App;
